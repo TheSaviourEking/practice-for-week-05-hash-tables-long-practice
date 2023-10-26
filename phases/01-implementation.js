debbe1ec6c1b1307d4d1e101dd2280ced55afd2d
Saviour Eking <saviour.eking@outlook.com>
2023-10-26 02:29:20 +0100

    SOLVED: fix 01-implementations.js

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -11,6 +11,12 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
   constructor(numBuckets = 8) {
     // Initialize your buckets here
     // Your code here
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
+    this.count = 0;
+
+    // memoizing using a map to store already computed keys and values;
+    this.keyValues = new Map();
   }
 
   hash(key) {
@@ -31,23 +37,85 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   insert(key, value) {
     // Your code here
+    let index = this.hashMod(key);
+    const keyValuePair = new KeyValuePair(key, value);
+    if (this.keyValues.has(key)) {
+      let current = this.data[index];
+      while (current && !(key === current.key)) {
+        current = current.next;
+      }
+      current.value = value;
+    } else {
+      keyValuePair.next = this.data[index];
+      this.data[index] = keyValuePair;
+      this.keyValues.set(key, value);
+      this.count++;
+    }
   }
 
 
   read(key) {
     // Your code here
+    let index = this.hashMod(key);
+    if (this.keyValues.has(key)) {
+      return this.keyValues.get(key)
+    }
+    return undefined;
   }
 
 
   resize() {
     // Your code here
+    let oldData = this.data;
+    this.capacity = this.capacity * 2;
+    this.count = 0;
+    this.data = new Array(this.capacity).fill(null);
+    this.keyValues.clear();
+    // this.keyValues = new Map();
+    for (let i = 0; i < oldData.length; i++) {
+      let current = oldData[i];
+      while (current) {
+        this.insert(current.key, current.value);
+        current = current.next
+      }
+    }
   }
 
 
   delete(key) {
     // Your code here
+    let index = this.hashMod(key);
+    if (this.keyValues.has(key)) {
+      let current = this.data[index];
+      let prev = null;
+      while (current && !(key === current.key)) {
+        prev = current;
+        current = current.next;
+      }
+      if (prev) {
+        prev.next = current.next
+      } else {
+        this.data[index] = current.next;
+      }
+      this.keyValues.delete(key);
+      this.count--;
+    } else {
+      return 'Key not found';
+    }
   }
 }
 
+let hashTable = new HashTable(2);
+hashTable.insert("key1", "value1")
+hashTable.insert("key2", "value2")
+hashTable.insert("key3", "value3")
+hashTable.insert("key5", "value5")
+hashTable.insert("key9", "value9")
+hashTable.insert("key10", "value10")
+// console.log(hashTable.data);
+// console.log(hashTable);
+// hashTable.resize();
+// hashTable.delete('key2');
+// console.log(hashTable)
 
 module.exports = HashTable;
